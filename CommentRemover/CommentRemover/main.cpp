980e53a69fe68e2587b2ad7401b351f92432405a
FormMe <formme1995@yandex.ru>
2017-12-12 02:22:43 +0300

    exception handling

--- CommentRemover/CommentRemover/main.cpp
+++ CommentRemover/CommentRemover/main.cpp
@@ -1,12 +1,19 @@
 #include "ArgvParser.h"
-
+#include <iostream>
 int	main(int argc, char* argv[])
 {
-	ArgvParser cmdParams;
-	cmdParams.Parse(argc, argv);
-	auto remover = CommentRemoverCreator::Create(cmdParams.lang);
-	remover->remove(cmdParams.getInputStream(),
-					cmdParams.getOutputStream());
+	try
+	{
+		ArgvParser cmdParams;
+		cmdParams.Parse(argc, argv);
+		auto remover = CommentRemoverCreator::Create(cmdParams.lang);
+		remover->remove(cmdParams.getInputStream(),
+			cmdParams.getOutputStream());
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << ex.what();
+	}
 
 	return 0;
 }
